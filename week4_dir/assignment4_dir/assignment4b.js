dd3a12358e2f1d7c93924c9c4858d678829bfbb1
Emanuel Langit <emanuel.langit@fastmail.fm>
2014-06-11 17:05:46 -0700

    assignment 4 completed up to step 5

--- week4_dir/assignment4_dir/assignment4b.js
+++ week4_dir/assignment4_dir/assignment4b.js
@@ -9,7 +9,31 @@ function analyzeText() {
 } // end of analyzeText function
 
 function countNumVowels(textInput) {
-	//alert("you entered: " + textInput);
+
 	var vowels = textInput.match(/[AEIOUaeiou]/g);
-	alert("The Number of vowels in " + textInput + " is " + vowels.length);
+	var aString = textInput.match(/[Aa]/g);
+	var eString = textInput.match(/[Ee]/g);
+	var iString = textInput.match(/[Ii]/g);
+	var oString = textInput.match(/[Oo]/g);
+	var uString = textInput.match(/[Uu]/g);
+	
+	/*
+	 * If there are no occurrences of each of the following,
+	 * then set the value zero, else, count the lengths
+	 */
+	var numberOfVowels = vowels != null ? vowels.length : 0;
+	var numberOfAs = vowels != null ? aString.length : 0;
+	var numberOfEs = eString != null ? eString.length : 0;	
+	var numberOfIs = iString != null ? iString.length : 0;
+	var numberOfOs = oString != null ? oString.length : 0;
+	var numberOfUs = uString != null ? uString.length : 0;
+	
+
+	document.getElementById("numberOfVowels").value = numberOfVowels;
+	document.getElementById("numberOfAs").value = numberOfAs;
+	document.getElementById("numberOfEs").value = numberOfEs;
+	document.getElementById("numberOfIs").value = numberOfIs;
+	document.getElementById("numberOfOs").value = numberOfOs;
+	document.getElementById("numberOfUs").value = numberOfUs;
+
 }
