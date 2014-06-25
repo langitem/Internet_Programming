2150b1f5feef4672067816133d8f78e014a3840a
Emanuel Langit <emanuel.langit@fastmail.fm>
2014-06-24 19:33:07 -0700

    Delete device functioning in assignment 4

--- week6_dir/assignment_dir/deleteDevice2.php
+++ week6_dir/assignment_dir/deleteDevice2.php
@@ -1,12 +1,15 @@
 <?php
 	$refNo = $_POST['refNo'];
+	/*
 	$name = $_POST['name']; 
 	$deviceType = $_POST['deviceType']; 
 	$colour = $_POST['colour']; 
 	$stockLevel = $_POST['stockLevel']; 
 	$salesThisMonth = $_POST['salesThisMonth'];
 	$customerRating = $_POST['customerRating'];
-	/* The above code retrieves data from the HTML form contained in updateDevice1.php */
+	*/
+	
+	/* The above code retrieves data from the HTML form contained in deleteDevice1.php */
 	/*
 	$dsn = 'mysql:host=laureatestudentserver.com;dbname=laureate_mobiles';
 	$username = 'laureate_xxxx';
@@ -15,13 +18,14 @@
 	*/
 	include("../../../connection.php");
 	 
-	$sqlQuery = "UPDATE devices SET name = '$name', deviceType = '$deviceType', colour = '$colour', stockLevel = $stockLevel, salesThisMonth = $salesThisMonth, customerRating = $customerRating  WHERE refNo = '$refNo'"; 
+	$sqlQuery = "UPDATE devices SET name = '$name', deviceType = '$deviceType', colour = '$colour', stockLevel = $stockLevel, salesThisMonth = $salesThisMonth, customerRating = $customerRating  WHERE refNo = '$refNo'";
+	$sqlQuery = "DELETE FROM devices where refNo = '$refNo'";
 	/* The SQL says "Update the devices table by setting each field to the contents of the HTML form in updateDevice1.php". Only where the user changed something will any difference be seen */
-	$update_result = $dbc->exec($sqlQuery);
-	/* Execute the SQL query and store the number of rows that were changed in $update_result */ 
+	$delete_result = $dbc->exec($sqlQuery);
+	/* Execute the SQL query and store the number of rows that were changed in $delete_result */ 
 	$dbc = null;
 	
-	if($update_result != 0)
+	if($delete_result != 0)
 	/* If at least one row in the database was changed */
 	{
 		echo("<h3>The device record was deleted successfully</h3>");
